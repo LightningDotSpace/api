f20982d527fbdfc6df336e2d2ede7ec05b901716
bernd2022 <bernd.nospam.2018@t-online.de>
2023-08-30 07:56:16 +0200

    [DEV-1688] remove LNURL calls from Swagger

--- src/subdomains/lightning/controllers/lightning-payment-forward.controller.ts
+++ src/subdomains/lightning/controllers/lightning-payment-forward.controller.ts
@@ -1,20 +1,14 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
-import { LnBitsLnurlPayRequestDto } from 'src/integration/blockchain/lightning/dto/lnbits.dto';
+import { ApiExcludeController, ApiTags } from '@nestjs/swagger';
 import { LightningForwardService } from '../services/lightning-forward.service';
 
 @ApiTags('LNURL')
 @Controller('lnurlp')
+@ApiExcludeController()
 export class LightingPaymentForwardController {
   constructor(private forwardService: LightningForwardService) {}
 
-  @Get(':id')
-  async lnUrlPForward(@Param('id') id: string): Promise<LnBitsLnurlPayRequestDto> {
-    return this.forwardService.lnurlpForward(id);
-  }
-
   @Get('cb/:id')
-  @ApiQuery({ name: 'amount', required: true, type: String })
   async lnUrlPCallbackForward(@Param('id') id: string, @Query() params: any): Promise<any> {
     return this.forwardService.lnurlpCallbackForward(id, params);
   }
