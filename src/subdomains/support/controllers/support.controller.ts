17d02319f27099cda13839d09162bd65c6d5ffc1
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-09-07 09:06:42 +0200

    [DEV-1780] add gs support route

---
+++ src/subdomains/support/controllers/support.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiExcludeEndpoint } from '@nestjs/swagger';
+import { RoleGuard } from 'src/shared/auth/role.guard';
+import { WalletRole } from 'src/shared/auth/wallet-role.enum';
+import { DbQueryDto } from '../dto/dp-query.dto';
+import { SupportService } from '../services/support.service';
+
+@Controller('support')
+export class SupportController {
+  constructor(private readonly supportService: SupportService) {}
+
+  @Post('db')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(WalletRole.ADMIN))
+  async getRawData(
+    @Body()
+    query: DbQueryDto,
+  ): Promise<{ keys: string[]; values: any }> {
+    return this.supportService.getRawData(query);
+  }
+}
---
+++ src/subdomains/support/services/support.service.ts
@@ -0,0 +1,63 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { DbQueryDto } from '../dto/dp-query.dto';
+
+@Injectable()
+export class SupportService {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async getRawData(query: DbQueryDto): Promise<any> {
+    const request = this.dataSource
+      .createQueryBuilder()
+      .from(query.table, query.table)
+      .orderBy(`${query.table}.id`, query.sorting)
+      .limit(query.maxLine)
+      .where(`${query.table}.id >= :id`, { id: query.min })
+      .andWhere(`${query.table}.updated >= :updated`, { updated: query.updatedSince });
+
+    if (query.select) request.select(query.select);
+
+    for (const where of query.where) {
+      request.andWhere(where[0], where[1]);
+    }
+
+    for (const join of query.join) {
+      request.leftJoin(join[0], join[1]);
+    }
+
+    const data = await request.getRawMany().catch((e: Error) => {
+      throw new BadRequestException(e.message);
+    });
+
+    // transform to array
+    return this.transformResultArray(data, query.table);
+  }
+
+  //*** HELPER METHODS ***//
+
+  private transformResultArray(
+    data: any[],
+    table: string,
+  ):
+    | {
+        keys: string[];
+        values: any;
+      }
+    | undefined {
+    // transform to array
+    return data.length > 0
+      ? {
+          keys: this.renameDbKeys(table, Object.keys(data[0])),
+          values: data.map((e) => Object.values(e)),
+        }
+      : undefined;
+  }
+
+  private renameDbKeys(table: string, keys: string[]): string[] {
+    return keys.map((k) => k.replace(`${table}_`, '')).map((k) => (k.includes('_') ? this.toDotSeparation(k) : k));
+  }
+
+  private toDotSeparation(str: string): string {
+    return str.charAt(0).toLowerCase() + str.slice(1).split('_').join('.');
+  }
+}
