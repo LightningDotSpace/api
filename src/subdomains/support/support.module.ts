f2ae9da3dee126322ce43b82ae269513f9e89085
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-09-07 16:23:28 +0200

    [DEV-1780] fix module

--- src/subdomains/support/support.module.ts
+++ src/subdomains/support/support.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { LightningModule } from 'src/integration/blockchain/lightning/lightning.module';
-import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupportController } from './controllers/support.controller';
 import { SupportService } from './services/support.service';
 
 @Module({
-  imports: [LightningModule, UserModule],
+  imports: [TypeOrmModule.forFeature()],
   controllers: [SupportController],
   providers: [SupportService],
   exports: [],
