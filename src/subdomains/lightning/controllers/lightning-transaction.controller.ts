13a25d4ea8627dcf75b225b8690b6023c17d3d28
bernd2022 <104787072+bernd2022@users.noreply.github.com>
2023-10-11 15:30:49 +0200

    [DEV-1840] get transaction by id (#16)
    * [DEV-1840] get transaction by id
    
    * [DEV-1840] changed balance detection
    
    * [DEV-1840] changes after review

---
+++ src/subdomains/lightning/controllers/lightning-transaction.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { LightningTransactionDto } from '../dto/lightning-transaction.dts';
+import { LightningTransactionService } from '../services/lightning-transaction.service';
+
+@ApiTags('Transaction')
+@Controller('lightning')
+export class LightingTransactionController {
+  constructor(private lightningTransactionService: LightningTransactionService) {}
+
+  @Get('tx/:id')
+  async getTransactionInfo(@Param('id') id: string): Promise<LightningTransactionDto[]> {
+    return this.lightningTransactionService.getTransactionInfo(id);
+  }
+}
--- src/subdomains/user/application/services/lightning-wallet.service.ts
+++ src/subdomains/user/application/services/lightning-wallet.service.ts
@@ -77,7 +77,7 @@ export class LightningWalletService {
   async processSyncLightningUserTransactions(): Promise<void> {
     if (Config.processDisabled(Process.UPDATE_LIGHTNING_USER_TRANSACTION)) return;
 
-    await this.syncLightningUserTransactions();
+    await this.syncLightningUserTransactions(undefined, undefined, undefined, true);
   }
 
   async syncLightningUserTransactions(
